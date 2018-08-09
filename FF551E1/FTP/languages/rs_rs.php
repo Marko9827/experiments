29cef16848dc140a54d6b7c71c8a20959ca9b73a
Marko Nikolic <marko.supergun@gmail.com>
2018-08-09 15:00:56 +0200

    aerear
    aer

---
+++ FF551E1/FTP/languages/rs_rs.php
@@ -0,0 +1,154 @@
+<?php
+
+// Language: English (US)
+// Author: Monsta Apps
+// Last Updated: 8 June, 2016
+
+$file_lang_name = "Serbian/Sprski (RS)";
+$file_lang_default = 1; // 1 for default, 0 for not
+
+$lang_title_ended = "Završena sesija";
+$lang_title_errors = "Greške nastale";
+
+$lang_session_expired = "Vaša sesija je istekla";
+$lang_ip_conflict = "Vaša IP adresa se konflikta sa IP sesijom";
+$lang_token_conflict = "Otkriven je mogući CSRF";
+
+$lang_btn_login = "Prijavite se";
+$lang_ftp_host = "FTP Host";
+$lang_port = "Port";
+$lang_passive_mode = "Pasivni režim (Podrazumevano)";
+$lang_username = "Korisničko ime";
+$lang_password = "Šifra";
+$lang_ftp_ssl = "Povežite se koristeći SSL (ako je dostupno)";
+$lang_adv_interface = "Napredni interfejs";
+$lang_save_login = "Sačuvaj podatke za prijavu";
+$lang_language = "Jezik";
+$lang_skin = "Izgled";
+$lang_ip_check = "Zaključaj sesiju na IP";
+
+$lang_skins_empty = "[izgled] folder prazan!";
+$lang_skins_locked = "[izgled] folder zaključan!";
+$lang_skins_missing = "[izgled] folder nedostaje!";
+
+$lang_missing_fields = "Greška! Molimo popunite sva označena polja";
+$lang_max_logins = "Prekoračili ste dozvoljene pokušaje prijave i bili su zaključani. Pokušajte ponovo u [n] minutu";
+$lang_cant_connect = "Greška! Ne mogu se povezati sa FTP hostom";
+$lang_cant_authenticate = "Greška! Nije moguće autentifikovati vaše prijavljivanje";
+$lang_ip_conflict = "Otkriven je IP konflikt";
+
+$lang_table_name = "Ime";
+$lang_table_size = "Veličina";
+$lang_table_date = "Datum";
+$lang_table_time = "Vreme";
+$lang_table_user = "Korisnik";
+$lang_table_group = "Grupa";
+$lang_table_perms = "Dozvole";
+
+$lang_size_b = "B";
+$lang_size_kb = "KB";
+$lang_size_mb = "MB";
+$lang_size_gb = "GB";
+$lang_size_tb = "TB";
+
+$lang_btn_refresh = "Osveži";
+$lang_btn_dl = "Preuzmi";
+$lang_btn_cut = "Iseci";
+$lang_btn_copy = "Kopiraj";
+$lang_btn_paste = "Nalepi";
+$lang_btn_rename = "Preminuj";
+$lang_btn_delete = "Obriši";
+$lang_btn_chmod = "Chmod";
+$lang_btn_logout = "Odjava";
+$lang_btn_save = "Sačuvaj";
+$lang_btn_close = "Zatvori";
+$lang_btn_cancel = "Odustani";
+$lang_btn_ok = "OK";
+$lang_btn_new_folder = "Novi Folder";
+$lang_btn_new_file = "Novi fajl";
+$lang_btn_upload_file = "Otpremi fajl";
+$lang_btn_upload_files = "Otpremi fajlove";
+$lang_btn_upload_repeat = "Oporavi otpremanje";
+$lang_btn_upload_folder = "Otpremi folder";
+
+$lang_info_version = "Verzija";
+$lang_info_host = "Host";
+$lang_info_user = "Korisnik";
+$lang_info_upload_limit = "Ograničenje otpreme";
+$lang_info_drag_drop = "Prevucite i opustite";
+
+$lang_xfer_file = "Fajl";
+$lang_xfer_size = "Veličina";
+$lang_xfer_progress = "Napredak";
+$lang_xfer_elapsed = "Vreme koje je proteklo";
+$lang_xfer_uploaded = "Otpremnjeno";
+$lang_xfer_rate = "Brzina prenosa";
+$lang_xfer_remain = "Preostalo vreme";
+
+$lang_move_conflict = "Fascikla odredišta je potfascikla izvorne fascikle";
+$lang_cant_rename = "Unable to rename [file]";
+$lang_cant_delete = "Unable to delete [file]";
+
+$lang_folder_exists = "Folder [folder] already exists";
+$lang_folder_doesnt_exist = "Folder [folder] doesn't exist";
+$lang_folder_cant_move = "Unable to move [folder]";
+$lang_folder_cant_delete = "Unable to delete [folder]";
+$lang_folder_cant_access = "Unable to access [folder]";
+$lang_folder_cant_make = "Unable to create [folder]";
+
+$lang_file_exists = "File [file] already exists";
+$lang_file_doesnt_exist = "File [file] doesn't exist";
+$lang_file_cant_move = "Unable to move [file]";
+$lang_file_cant_make = "Couldn't create [file]";
+
+$lang_server_error_down = "Server error downloading [file]";
+$lang_server_error_up = "Server error uploading [file]";
+$lang_browser_error_up = "Browser error uploading [file]";
+$lang_file_size_error = "File size exceeds allowed limit";
+$lang_file_size_copy_error = "[file] size exceeds allowed limit";
+
+$lang_folder_cant_chmod = "Unable to chmod [perms] [folder]";
+$lang_file_cant_chmod = "Unable to chmod [perms] [file]";
+$lang_chmod_max_777 = "Chmod cannot be higher than 777";
+$lang_chmod_owner = "Owner Permissions";
+$lang_chmod_group = "Group Permissions";
+$lang_chmod_public = "Public Permissions";
+$lang_chmod_manual = "Manual";
+$lang_chmod_read = "Read";
+$lang_chmod_write = "Write";
+$lang_chmod_exe = "Execute";
+
+$lang_title_rename = "Rename Files";
+$lang_title_chmod = "Change File Attributes";
+$lang_title_edit_file = "Editing";
+$lang_title_new_file = "New File";
+$lang_title_new_folder = "New Folder";
+$lang_new_folder_name = "Enter new folder name...";
+$lang_new_file_name = "Enter new file name...";
+$lang_template = "Template";
+$lang_no_template = "No Template";
+
+$lang_no_xmlhttp = "Your browser does not support XMLHTTP!";
+$lang_support_drop = "Supported";
+$lang_no_support_drop = "Not Supported";
+$lang_transfer_pending = "Pending";
+$lang_transferring_to_ftp = "Transferring to FTP server...";
+$lang_no_file_selected = "Please click BROWSE and choose a file to upload!";
+$lang_none_selected = "Please select at least one file or folder!";
+$lang_context_open = "Open";
+$lang_context_download = "Download";
+$lang_context_edit = "Edit";
+$lang_context_cut = "Cut";
+$lang_context_copy = "Copy";
+$lang_context_paste = "Paste";
+$lang_context_rename = "Rename";
+$lang_context_delete = "Delete";
+$lang_context_chmod = "Chmod";
+
+$lang_title_fetch_file = "Fetch File";
+$lang_fetch_no_file = "The URL does not contain a file name.";
+$lang_fetch_not_found = "No file could be found at this address.";
+$lang_btn_fetch = "Fetch";
+$lang_btn_fetch_file = "Fetch File";
+
+?>
